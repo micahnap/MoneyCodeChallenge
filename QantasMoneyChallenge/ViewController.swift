2ca07183c26449e9dc7b05f058392ad0655d8f5b
Micah Napier <micah.napier@qantas.com.au>
2019-12-14 13:21:58 +1100

    Initial Commit

---
+++ QantasMoneyChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QantasMoneyChallenge
+//
+//  Created by Micah Napier on 14/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
